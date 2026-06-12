c3f6c7b284d92eb57965c34acf07d70b765a73be
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2026-06-12 17:18:16 -0400

    Commands test,detect, and pig added, established connection to local MongoDB

---
+++ logs/databse.go
@@ -0,0 +1,31 @@
+package logs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	db := client.Database("seapig")
+	fmt.Println("Using database:", db.Name())
+}
